1ac747937cbc159b07fb016cbc3641733b604f90
Yohan Graterol <yohangraterol92@gmail.com>
2017-04-25 21:18:52 -0500

    Funciones

--- main.go
+++ main.go
@@ -8,15 +8,25 @@ const testConst = "Test"
 func main() {
 	var name string
 	lastName := "<Modificar con mi apellido>"
-	var number = 100
-	var (
-		a = 1
-		b = 2
-		c = 3
-	)
-	fmt.Print("Ingresa tu nombre: ")
-	fmt.Scanf("%s", &name)
+	var number = sum(50, 50)
+	a, b, c := getVariables()
+	name = getName()
 	fmt.Printf(helloWorld, name, lastName)
 	fmt.Println("* Esta es otra forma de imprimir con un salto de línea")
 	fmt.Println(number, a, b, c, testConst)
+}
+
+func getName() string {
+	var name string
+	fmt.Print("Ingresa tu nombre: ")
+	fmt.Scanf("%s", &name)
+	return name
+}
+
+func getVariables() (int, int, int) {
+	return 1, 2, 3
+}
+
+func sum(a int, b int) int {
+	return a + b
 }
