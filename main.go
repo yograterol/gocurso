8931dd1e47619bb27c7b7dd6cdc8ce1888058cd2
Yohan Graterol <yohangraterol92@gmail.com>
2017-04-25 21:44:49 -0500

    Enteros y flotantes

--- main.go
+++ main.go
@@ -10,9 +10,15 @@ func main() {
 	lastName := "<Modificar con mi apellido>"
 	var number = sum(50, 50)
 	a, b, c := getVariables()
+	fa, fb := getVariablesFloat()
 	name = getName()
 	fmt.Printf(helloWorld, name, lastName)
 	fmt.Println("* Esta es otra forma de imprimir con un salto de línea")
+	fmt.Println("int: Número entero de al menos 32 bits en adelante", a)
+	fmt.Println("int32: Número entero de máximo 32 bits", b)
+	fmt.Println("int32: Número entero de máximo 64 bits", c)
+	fmt.Println("float32: Número flotante con precisión de 32 bits", fa)
+	fmt.Println("float64: Número flotante con precisión de 64 bits", fb)
 	fmt.Println(number, a, b, c, testConst)
 }
 
@@ -23,8 +29,12 @@ func getName() string {
 	return name
 }
 
-func getVariables() (int, int, int) {
-	return 1, 2, 3
+func getVariables() (int, int32, int64) {
+	return 9223372036854775807, 2147483647, 9223372036854775807
+}
+
+func getVariablesFloat() (float32, float64) {
+	return float32(0.1), float64(float32(0.1))
 }
 
 func sum(a int, b int) int {
