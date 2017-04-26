9e8102cef10e8bf259b8f94f2ca7a35dd2b8a636
Yohan Graterol <yohangraterol92@gmail.com>
2017-04-25 20:46:50 -0500

    Variables y constantes

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 
+const helloWorld string = "Hola %s %s, bienvenido al fascinante mundo de Go.\n"
+const testConst = "Test"
+
 func main() {
 	var name string
+	lastName := "<Modificar con mi apellido>"
+	var number = 100
+	var (
+		a = 1
+		b = 2
+		c = 3
+	)
 	fmt.Print("Ingresa tu nombre: ")
 	fmt.Scanf("%s", &name)
-	fmt.Printf("Hola %s, bienvenido al fascinante mundo de Go.\n", name)
+	fmt.Printf(helloWorld, name, lastName)
 	fmt.Println("* Esta es otra forma de imprimir con un salto de línea")
+	fmt.Println(number, a, b, c, testConst)
 }
