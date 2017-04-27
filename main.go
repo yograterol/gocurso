4b508cd29be52c30277f1cd96c749f6e7a7adadc
Yohan Graterol <yohangraterol92@gmail.com>
2017-04-27 11:59:15 -0500

    Update main.go

--- main.go
+++ main.go
@@ -7,5 +7,5 @@ func main() {
 	fmt.Print("Ingresa tu nombre: ")
 	fmt.Scanf("%s", &name)
 	fmt.Printf("Hola %s, bienvenido al fascinante mundo de Go.\n", name)
-	fmt.Println("* Esta es otra forma de imprimir con un salto de línea")
+	fmt.Println("Hola mundo")
 }
